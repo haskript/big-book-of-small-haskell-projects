6f6097bbd36b5b23c1e3bd347bbb7af8cc9e3326
liamzee <liam.zhu@protonmail.com>
2023-10-25 05:23:26 +0800

    Reverted Clickbait to Naive version
    Reverted Clickbait to naive version because even though
    it's smelly, it's still what I feel most comfortable with.

--- 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
+++ 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
@@ -1,64 +1,40 @@
 {- cabal:
 default-language: GHC2021
-build-depends: base, random, vector, text, text-show, transformers
+build-depends: base, random
 -}
-{-# LANGUAGE OverloadedLists, OverloadedStrings, TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Vector (Vector)
-import Data.Vector qualified as V
 import System.Random (initStdGen, randomR, StdGen)
 import Data.String (IsString, fromString)
-import TextShow (showt)
 import Text.Read (readMaybe)
-import Data.Text (Text, toLower, pack)
-import Data.Text.IO qualified as TIO
 import Control.Monad (join, replicateM_)
-import Data.Functor ((<&>))
-import Data.Foldable (traverse_)
 import System.IO (hFlush, stdout)
-import Control.Monad.Trans.State.Strict
-    ( State
-    , evalState
-    , get
-    , put
-    , modify' )
-import Control.Arrow (first)
+import Data.Char (toLower)
 
-type RndState = State StdGen
+instance IsString (IO String) where
+    fromString = pure
 
-rndStateToIO :: State StdGen a -> IO a
-rndStateToIO term = evalState term <$> initStdGen
+randomWords :: [[String]]
+randomWords@[ objectPronouns, possessivePronouns, personalPronouns
+            , states, nouns, places, when, websites ] =
+  [ ["Her", "Him", "Them"], ["Her", "His", "Their"], ["She", "He", "They"]
+  , [ "California", "Texas", "Florida", "New York", "Pennsylvania"
+    , "Illinois", "Ohio", "Georgia", "North Carolina", "Michigan" ]
+  , [ "Athlete", "Clown", "Shovel", "Paleo Diet", "Doctor", "Parent"
+    , "Cat", "Dog", "Chicken", "Robot", "Video Game", "Avocado"
+    , "Plastic Straw", "Serial Killer", "Telephone Psychic" ]
+  , [ "House", "Attic", "Bank Deposit Box", "School", "Basement"
+    , "Workplace", "Donut Shop", "Apocalypse Bunker" ]
+  , [ "Soon", "This Year", "Later Today", "RIGHT NOW", "Next Week"]
+  , [ "wobsite", "blag", "Facebuuk", "Googles", "Facesbook", "Tweedie"
+    , "Pastagram"] ]
 
-instance IsString (RndState Text) where
-    fromString = pure . pack
+selectors :: [IO String]
+selectors@[ objectPronoun, possessivePronoun, personalPronoun, state
+          , noun, place, aWhen, website ] = select <$> randomWords
 
-instance Semigroup a => Semigroup (RndState a) where
-    a <> b = liftA2 (<>) a b
-
-objectPronouns, possessivePronouns, personalPronouns,
-    states, nouns, places, when,
-    websites :: Vector Text
-objectPronouns = [ "Her", "Him", "Them" ]
-possessivePronouns = [ "Her", "His", "Their" ]
-personalPronouns = [ "She", "He", "They" ]
-states = [ "California", "Texas", "Florida", "New York", "Pennsylvania"
-         , "Illinois", "Ohio", "Georgia", "North Carolina", "Michigan" ]
-nouns = [ "Athlete", "Clown", "Shovel", "Paleo Diet", "Doctor", "Parent"
-        , "Cat", "Dog", "Chicken", "Robot", "Video Game", "Avocado"
-        , "Plastic Straw", "Serial Killer", "Telephone Psychic" ]
-places = [ "House", "Attic", "Bank Deposit Box", "School", "Basement"
-         , "Workplace", "Donut Shop", "Apocalypse Bunker" ]
-when = [ "Soon", "This Year", "Later Today", "RIGHT NOW", "Next Week"]
-websites = [ "wobsite", "blag", "Facebuuk", "Googles", "Facesbook", "Tweedie"
-           , "Pastagram"]
-
-funcBatch :: Vector (RndState Text)
-funcBatch@[ noun, aWhen, objectPronoun, state, possessivePronoun, place
-    , personalPronoun, website ] = select <$> [ nouns, when, objectPronouns,
-    states, possessivePronouns, places, personalPronouns, websites ]
-
-intro :: Vector Text
+intro :: [String]
 intro =
     [ "Clickbait Headline Generator"
     , "By Liam Zhu liam.zhu@protonmail.com, adapted from"
@@ -67,55 +43,49 @@ intro =
     , "Our website needs to trick people into looking at ads!"
     ]
 
-headlines :: Vector (RndState Text)
+headlines :: [IO String]
 headlines =
-    [ "Are Millenials Killing the " <> noun <> " Industry?" 
+    [ "Are Millenials Killing the " <> noun <> " Industry?"
     , "Without This " <> noun <> ", " <> noun <> "s Could Kill You " <> aWhen
     , "Big Companies Hate " <> objectPronoun <> "! See How This " <> state
-          <> " " <> noun <> " Invented a Cheaper " <> noun
+      <> " " <>  noun <> " Invented a Cheaper " <> noun
     , "You Won't Believe What This " <> state <> " " <> noun <> " Found in "
-          <> possessivePronoun <> " " <> place
+      <> possessivePronoun <> " " <> place
     , "What " <> noun <> "s Don't Want You To Know About " <> noun <> "s"
-    , (showt <$> selectNumber 7 15 ) <> " Gift Ideas to Give Your " <> noun
-          <> " From " <> state
-    , do initialNumber <- selectNumber 3 19
-         pure (showt initialNumber) <> " Reasons Why " <> noun
+    , (show <$> selectNumber 7 15) <> " Gift Ideas to Give Your " <> noun
+      <> " From " <> state
+    , do reasonNum <- selectNumber 3 19
+         pure (show reasonNum) <> " Reasons Why " <> noun
              <> " Are More Interesting Than You Think (Number "
-             <> (showt <$> selectNumber 1 initialNumber)
-	     <> " Will Surprise You!)"
+             <> (show <$> selectNumber 1 reasonNum) <> " Will Surprise You!)"
     , do num <- selectNumber 0 2
          "This " <> state <> " " <> noun <> " Didn't Think Robots Would Take "
-	     <> (pure $ possessivePronouns V.! num) <> " Job. "
-	     <> (pure $ personalPronouns V.! num) <> " "
+             <> (pure $ possessivePronouns !! num) <> " Job. "
+             <> (pure $ personalPronouns !! num) <> " "
 	     <> if num == 2 then "Were" else "Was" <> " Wrong"
     ]
 
-select :: Vector a -> RndState a
-select vec = do
-    gen <- get
-    let (output, newGen) = first (vec V.!) $ randomR (0, V.length vec - 1) gen
-    put newGen
-    pure output
+select :: [a] -> IO a
+select ls = (ls !!) . fst . randomR (0, length ls - 1) <$> initStdGen
 
-selectNumber :: Int -> Int -> RndState Int
-selectNumber lower upper = do
-    gen <- get
-    let (output, newGen) = randomR (lower, upper) gen
-    put newGen
-    pure output
+selectNumber :: Int -> Int -> IO Int
+selectNumber lower upper = fst . randomR (lower, upper) <$> initStdGen
 
 main :: IO ()
-main = traverse_ TIO.putStrLn intro >> inputLoop
+main = mapM_ putStrLn intro >> inputLoop
 
 inputLoop :: IO ()
 inputLoop = do
-    TIO.putStrLn "Enter the number of clickbait headlines to generate:"
-    number <- TIO.putStr "> " >> hFlush stdout >> getLine
+    putStrLn "Enter the number of clickbait headlines to generate:"
+    number <- do putStr "> "
+                 hFlush stdout
+		 getLine
+
     case readMaybe number :: Maybe Int of
-        Nothing -> TIO.putStrLn "Please enter a number." >> inputLoop
+        Nothing -> putStrLn "Please enter a number." >> inputLoop
 	Just count -> do
             replicateM_ count
-	        $ TIO.putStrLn =<< rndStateToIO ( join $ select headlines )
-	    TIO.putStrLn ""
-	    TIO.putStrLn =<< rndStateToIO ("Post these to our " <> website
-	        <> " " <> fmap toLower aWhen <> " or you're fired!")
+	        $ putStrLn =<< ( join $ select headlines )
+	    putStrLn ""
+	    putStrLn =<< "Post these to our " <> website <> " "
+	        <> (fmap . fmap) toLower aWhen <> " or you're fired!"
