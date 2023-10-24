4789c6c79d005524466703f24ef4495949733a1e
liamzee <liam.zhu@protonmail.com>
2023-10-25 03:27:11 +0800

    Refactored to use State type with gen passer
    Refactored to use RndState type (State StdGen) instead of
    IO in my code, as well as to use Text.
    
    I hope the Haskell community is happy.

--- 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
+++ 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
@@ -1,24 +1,44 @@
 {- cabal:
 default-language: GHC2021
-build-depends: base, random, vector
+build-depends: base, random, vector, text, text-show, transformers
 -}
 {-# LANGUAGE OverloadedLists, OverloadedStrings, TypeApplications #-}
 module Main where
 
 import Data.Vector (Vector)
 import Data.Vector qualified as V
-import System.Random (initStdGen, randomR)
+import System.Random (initStdGen, randomR, StdGen)
 import Data.String (IsString, fromString)
+import TextShow (showt)
 import Text.Read (readMaybe)
+import Data.Text (Text, toLower, pack)
+import Data.Text.IO qualified as TIO
 import Control.Monad (join, replicateM_)
 import Data.Functor ((<&>))
 import Data.Foldable (traverse_)
 import System.IO (hFlush, stdout)
-import Data.Char (toLower)
+import Control.Monad.Trans.State.Strict
+    ( State
+    , evalState
+    , get
+    , put
+    , modify' )
+import Control.Arrow (first)
+
+type RndState = State StdGen
+
+rndStateToIO :: State StdGen a -> IO a
+rndStateToIO term = evalState term <$> initStdGen
+
+instance IsString (RndState Text) where
+    fromString = pure . pack
+
+instance Semigroup a => Semigroup (RndState a) where
+    a <> b = liftA2 (<>) a b
 
 objectPronouns, possessivePronouns, personalPronouns,
     states, nouns, places, when,
-    websites :: Vector String
+    websites :: Vector Text
 objectPronouns = [ "Her", "Him", "Them" ]
 possessivePronouns = [ "Her", "His", "Their" ]
 personalPronouns = [ "She", "He", "They" ]
@@ -33,12 +53,12 @@ when = [ "Soon", "This Year", "Later Today", "RIGHT NOW", "Next Week"]
 websites = [ "wobsite", "blag", "Facebuuk", "Googles", "Facesbook", "Tweedie"
            , "Pastagram"]
 
-funcBatch :: Vector (IO String)
+funcBatch :: Vector (RndState Text)
 funcBatch@[ noun, aWhen, objectPronoun, state, possessivePronoun, place
     , personalPronoun, website ] = select <$> [ nouns, when, objectPronouns,
     states, possessivePronouns, places, personalPronouns, websites ]
 
-intro :: Vector String
+intro :: Vector Text
 intro =
     [ "Clickbait Headline Generator"
     , "By Liam Zhu liam.zhu@protonmail.com, adapted from"
@@ -47,10 +67,7 @@ intro =
     , "Our website needs to trick people into looking at ads!"
     ]
 
-instance IsString (IO String) where
-    fromString = pure
-
-headlines :: Vector (IO String)
+headlines :: Vector (RndState Text)
 headlines =
     [ "Are Millenials Killing the " <> noun <> " Industry?" 
     , "Without This " <> noun <> ", " <> noun <> "s Could Kill You " <> aWhen
@@ -59,12 +76,12 @@ headlines =
     , "You Won't Believe What This " <> state <> " " <> noun <> " Found in "
           <> possessivePronoun <> " " <> place
     , "What " <> noun <> "s Don't Want You To Know About " <> noun <> "s"
-    , (show <$> selectNumber 7 15 ) <> " Gift Ideas to Give Your " <> noun
+    , (showt <$> selectNumber 7 15 ) <> " Gift Ideas to Give Your " <> noun
           <> " From " <> state
     , do initialNumber <- selectNumber 3 19
-         pure (show initialNumber) <> " Reasons Why " <> noun
+         pure (showt initialNumber) <> " Reasons Why " <> noun
              <> " Are More Interesting Than You Think (Number "
-             <> (show <$> selectNumber 1 initialNumber)
+             <> (showt <$> selectNumber 1 initialNumber)
 	     <> " Will Surprise You!)"
     , do num <- selectNumber 0 2
          "This " <> state <> " " <> noun <> " Didn't Think Robots Would Take "
@@ -73,26 +90,32 @@ headlines =
 	     <> if num == 2 then "Were" else "Was" <> " Wrong"
     ]
 
-select :: Vector a -> IO a
-select vec = initStdGen
-    <&> (vec V.!) . fst . randomR (0, V.length vec - 1)
+select :: Vector a -> RndState a
+select vec = do
+    gen <- get
+    let (output, newGen) = first (vec V.!) $ randomR (0, V.length vec - 1) gen
+    put newGen
+    pure output
 
-selectNumber :: Int -> Int -> IO Int
-selectNumber lower upper = initStdGen
-    <&> fst . randomR (lower, upper)
+selectNumber :: Int -> Int -> RndState Int
+selectNumber lower upper = do
+    gen <- get
+    let (output, newGen) = randomR (lower, upper) gen
+    put newGen
+    pure output
 
 main :: IO ()
-main = traverse_ putStrLn intro >> inputLoop
+main = traverse_ TIO.putStrLn intro >> inputLoop
 
 inputLoop :: IO ()
 inputLoop = do
-    putStrLn "Enter the number of clickbait headlines to generate:"
-    number <- putStr "> " >> hFlush stdout >> getLine
+    TIO.putStrLn "Enter the number of clickbait headlines to generate:"
+    number <- TIO.putStr "> " >> hFlush stdout >> getLine
     case readMaybe number :: Maybe Int of
-        Nothing -> putStrLn "Please enter a number." >> inputLoop
+        Nothing -> TIO.putStrLn "Please enter a number." >> inputLoop
 	Just count -> do
             replicateM_ count
-	        $ putStrLn =<< ( join $ select headlines )
-	    putStrLn ""
-	    putStrLn =<< "Post these to our " <> website <> " "
-	        <> fmap (toLower <$>) aWhen <> " or you're fired!"
+	        $ TIO.putStrLn =<< rndStateToIO ( join $ select headlines )
+	    TIO.putStrLn ""
+	    TIO.putStrLn =<< rndStateToIO ("Post these to our " <> website
+	        <> " " <> fmap toLower aWhen <> " or you're fired!")
