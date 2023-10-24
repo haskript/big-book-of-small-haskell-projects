6fbffa5325795b477c903104f4b04fc458eabfb6
liamzee <liam.zhu@protonmail.com>
2023-10-25 05:46:39 +0800

    Update 11 Clickbait with Golfed Version
    I updated 11 Clickbait with more golfing.
    The current version fits only in 84 lines, and is probably
    appropriately simple to the problem.
    
    Unfortunately, it is making very aggressive use of the IO
    type, so it's not really idiomatic.

--- 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
+++ 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
@@ -2,18 +2,13 @@
 default-language: GHC2021
 build-depends: base, random
 -}
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System.Random (initStdGen, randomR, StdGen)
-import Data.String (IsString, fromString)
+import System.Random (initStdGen, randomR)
 import Text.Read (readMaybe)
 import Control.Monad (join, replicateM_)
 import System.IO (hFlush, stdout)
 import Data.Char (toLower)
-
-instance IsString (IO String) where
-    fromString = pure
 
 randomWords :: [[String]]
 randomWords@[ objectPronouns, possessivePronouns, personalPronouns
@@ -35,35 +30,36 @@ selectors@[ objectPronoun, possessivePronoun, personalPronoun, state
           , noun, place, aWhen, website ] = select <$> randomWords
 
 intro :: [String]
-intro =
-    [ "Clickbait Headline Generator"
-    , "By Liam Zhu liam.zhu@protonmail.com, adapted from"
-    , "original Python version by Al Sweigart al@inventwithppython.com"
-    , ""
-    , "Our website needs to trick people into looking at ads!"
-    ]
+intro = [ "Clickbait Headline Generator"
+        , "By Liam Zhu liam.zhu@protonmail.com, adapted from"
+        , "original Python version by Al Sweigart al@inventwithppython.com"
+        , ""
+        , "Our website needs to trick people into looking at ads!" ]
 
 headlines :: [IO String]
 headlines =
-    [ "Are Millenials Killing the " <> noun <> " Industry?"
-    , "Without This " <> noun <> ", " <> noun <> "s Could Kill You " <> aWhen
-    , "Big Companies Hate " <> objectPronoun <> "! See How This " <> state
-      <> " " <>  noun <> " Invented a Cheaper " <> noun
-    , "You Won't Believe What This " <> state <> " " <> noun <> " Found in "
-      <> possessivePronoun <> " " <> place
-    , "What " <> noun <> "s Don't Want You To Know About " <> noun <> "s"
-    , (show <$> selectNumber 7 15) <> " Gift Ideas to Give Your " <> noun
-      <> " From " <> state
+    [ pure "Are Millenials Killing the " <> noun <> pure " Industry?"
+    , pure "Without This " <> noun <> pure ", " <> noun
+      <> pure "s Could Kill You " <> aWhen
+    , pure "Big Companies Hate " <> objectPronoun <> pure "! See How This " 
+      <> state <> pure " " <> noun <> pure " Invented a Cheaper " <> noun
+    , pure "You Won't Believe What This " <> state <> pure " " <> noun
+      <> pure " Found in " <> possessivePronoun <> pure " " <> place
+    , pure "What " <> noun <> pure "s Don't Want You To Know About " <> noun 
+      <> pure "s"
+    , (show <$> selectNumber 7 15) <> pure " Gift Ideas to Give Your " <> noun
+      <> pure " From " <> state
     , do reasonNum <- selectNumber 3 19
-         pure (show reasonNum) <> " Reasons Why " <> noun
-             <> " Are More Interesting Than You Think (Number "
-             <> (show <$> selectNumber 1 reasonNum) <> " Will Surprise You!)"
+         pure (show reasonNum) <> pure " Reasons Why " <> noun
+             <> pure " Are More Interesting Than You Think (Number "
+             <> (show <$> selectNumber 1 reasonNum)
+             <> pure " Will Surprise You!)"
     , do num <- selectNumber 0 2
-         "This " <> state <> " " <> noun <> " Didn't Think Robots Would Take "
-             <> (pure $ possessivePronouns !! num) <> " Job. "
-             <> (pure $ personalPronouns !! num) <> " "
-	     <> if num == 2 then "Were" else "Was" <> " Wrong"
-    ]
+         pure "This " <> state <> pure " " <> noun
+             <> pure " Didn't Think Robots Would Take "
+             <> (pure $ possessivePronouns !! num) <> pure " Job. "
+             <> (pure $ personalPronouns !! num) <> pure " "
+             <> pure (if num == 2 then "Were" else "Was") <> pure " Wrong" ]
 
 select :: [a] -> IO a
 select ls = (ls !!) . fst . randomR (0, length ls - 1) <$> initStdGen
@@ -77,15 +73,12 @@ main = mapM_ putStrLn intro >> inputLoop
 inputLoop :: IO ()
 inputLoop = do
     putStrLn "Enter the number of clickbait headlines to generate:"
-    number <- do putStr "> "
-                 hFlush stdout
-		 getLine
-
+    number <- putStr "> " >> hFlush stdout >> getLine
     case readMaybe number :: Maybe Int of
         Nothing -> putStrLn "Please enter a number." >> inputLoop
-	Just count -> do
+        Just count -> do
             replicateM_ count
-	        $ putStrLn =<< ( join $ select headlines )
-	    putStrLn ""
-	    putStrLn =<< "Post these to our " <> website <> " "
-	        <> (fmap . fmap) toLower aWhen <> " or you're fired!"
+                $ putStrLn =<< ( join $ select headlines )
+            putStrLn ""
+            putStrLn =<< pure "Post these to our " <> website <> pure " "
+                <> (fmap . fmap) toLower aWhen <> pure " or you're fired!"
