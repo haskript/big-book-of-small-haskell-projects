dd382e118c3b85347dd330f29506faf16f4e10ed
liamzee <liam.zhu@protonmail.com>
2023-10-25 08:11:55 +0800

    Rewrite Clickbait to be more idiomatic
    Rewrote Clickbait to be more idiomatic by using a
    custom state type with support for stdgen. Issues with
    superfluous pures were handled by turning strings into
    lambdas written in a list of strings style, using fold
    as needed to compress the list.
    
    Still not written for readability, but the code is of
    reasonable quality.

--- 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
+++ 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
@@ -1,14 +1,21 @@
 {- cabal:
 default-language: GHC2021
-build-depends: base, random
+build-depends: base, random, transformers
 -}
 module Main where
 
-import System.Random (initStdGen, randomR)
+import System.Random (initStdGen, randomR, StdGen)
 import Text.Read (readMaybe)
 import Control.Monad (join, replicateM_)
 import System.IO (hFlush, stdout)
 import Data.Char (toLower)
+import Data.Foldable (fold)
+import Control.Monad.Trans.State.Strict (State, evalState, get, put)
+
+type RndState = State StdGen
+
+rndStateToIO :: RndState a -> IO a
+rndStateToIO rnd = evalState rnd <$> initStdGen
 
 randomWords :: [[String]]
 randomWords@[ objectPronouns, possessivePronouns, personalPronouns
@@ -25,7 +32,7 @@ randomWords@[ objectPronouns, possessivePronouns, personalPronouns
   , [ "wobsite", "blag", "Facebuuk", "Googles", "Facesbook", "Tweedie"
     , "Pastagram"] ]
 
-selectors :: [IO String]
+selectors :: [RndState String]
 selectors@[ objectPronoun, possessivePronoun, personalPronoun, state
           , noun, place, aWhen, website ] = select <$> randomWords
 
@@ -36,36 +43,43 @@ intro = [ "Clickbait Headline Generator"
         , ""
         , "Our website needs to trick people into looking at ads!" ]
 
-headlines :: [IO String]
-headlines =
-    [ pure "Are Millenials Killing the " <> noun <> pure " Industry?"
-    , pure "Without This " <> noun <> pure ", " <> noun
-      <> pure "s Could Kill You " <> aWhen
-    , pure "Big Companies Hate " <> objectPronoun <> pure "! See How This " 
-      <> state <> pure " " <> noun <> pure " Invented a Cheaper " <> noun
-    , pure "You Won't Believe What This " <> state <> pure " " <> noun
-      <> pure " Found in " <> possessivePronoun <> pure " " <> place
-    , pure "What " <> noun <> pure "s Don't Want You To Know About " <> noun 
-      <> pure "s"
-    , (show <$> selectNumber 7 15) <> pure " Gift Ideas to Give Your " <> noun
-      <> pure " From " <> state
+headlines :: [RndState String]
+headlines = fmap fold <$>
+    [ (\noun -> ["Are Millenials Killing the ", noun, " Industry?"])
+        <$> noun
+    , (\noun1 noun2 when -> ["Without This ", noun1, ", ", noun2
+      , "s Could Kill You ", when] ) <$> noun <*> noun <*> aWhen
+    , (\objectPronoun state noun1 noun2 -> ["Big Companies Hate "
+      , objectPronoun, "! See How This ", state, " ", noun1
+      , " Invented a Cheaper ", noun2 ] ) <$> objectPronoun <*> state 
+        <*> noun <*> noun
+    , (\state noun possessivePronoun place -> ["You Won't Believe What This "
+      , state, " ", noun, " Found in ", possessivePronoun, " ", place])
+        <$> state <*> noun <*> possessivePronoun <*> place
+    , (\noun1 noun2 -> ["What ", noun1, "s Don't Want You To Know About "
+      , noun2, "s"]) <$> noun <*> noun
+    , (\number noun state -> [show number, " Gift Ideas to Give Your ", noun
+      , " From ", state] ) <$> selectNumber 7 15 <*> noun <*> state
     , do reasonNum <- selectNumber 3 19
-         pure (show reasonNum) <> pure " Reasons Why " <> noun
-             <> pure " Are More Interesting Than You Think (Number "
-             <> (show <$> selectNumber 1 reasonNum)
-             <> pure " Will Surprise You!)"
+         (\noun num2 -> [ show reasonNum, " Reasons Why ", noun
+             , " Are More Interesting Than You Think (Number "
+             , show num2, " Will Surprise You!)" ] )
+	     <$> noun <*> selectNumber 1 reasonNum
     , do num <- selectNumber 0 2
-         pure "This " <> state <> pure " " <> noun
-             <> pure " Didn't Think Robots Would Take "
-             <> (pure $ possessivePronouns !! num) <> pure " Job. "
-             <> (pure $ personalPronouns !! num) <> pure " "
-             <> pure (if num == 2 then "Were" else "Was") <> pure " Wrong" ]
+         (\state noun -> ["This ", state, " ", noun
+             , " Didn't Think Robots Would Take "
+             , possessivePronouns !! num, " Job. "
+             , personalPronouns !! num, " "
+             , if num == 2 then "Were" else "Was", " Wrong" ] )
+	     <$> state <*> noun ]
 
-select :: [a] -> IO a
-select ls = (ls !!) . fst . randomR (0, length ls - 1) <$> initStdGen
+select :: [a] -> RndState a
+select ls =
+    get >>= fmap (ls !!) . uncurry (<$) . fmap put . randomR (0, length ls - 1)
 
-selectNumber :: Int -> Int -> IO Int
-selectNumber lower upper = fst . randomR (lower, upper) <$> initStdGen
+selectNumber :: Int -> Int -> RndState Int
+selectNumber lower upper = get
+    >>= uncurry (<$) . fmap put . randomR (lower, upper)
 
 main :: IO ()
 main = mapM_ putStrLn intro >> inputLoop
@@ -78,7 +92,8 @@ inputLoop = do
         Nothing -> putStrLn "Please enter a number." >> inputLoop
         Just count -> do
             replicateM_ count
-                $ putStrLn =<< ( join $ select headlines )
+                $ putStrLn =<< ( rndStateToIO . join $ select headlines )
             putStrLn ""
-            putStrLn =<< pure "Post these to our " <> website <> pure " "
-                <> (fmap . fmap) toLower aWhen <> pure " or you're fired!"
+            putStrLn =<< ( rndStateToIO $ fmap fold ( (\website when ->
+	        [ "Post these to our ", website, " ", toLower <$> when, 
+		" or you're fired!" ] ) <$> website <*> aWhen ) )
