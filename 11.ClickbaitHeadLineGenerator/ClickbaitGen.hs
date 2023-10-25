d8d79d96bb3080f3cd107b33b21c87eeea3e2b7f
liamzee <liam.zhu@protonmail.com>
2023-10-25 09:20:48 +0800

    Modify Clickbait for readability
    Clickbait was modified for readibility, increasing the overall code length.

--- 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
+++ 11.ClickbaitHeadLineGenerator/ClickbaitGen.hs
@@ -1,6 +1,6 @@
 {- cabal:
 default-language: GHC2021
-build-depends: base, random, transformers
+build-depends: base, random, transformers, containers
 -}
 module Main where
 
@@ -10,7 +10,9 @@ import Control.Monad (join, replicateM_)
 import System.IO (hFlush, stdout)
 import Data.Char (toLower)
 import Data.Foldable (fold)
+import Data.Functor ((<&>))
 import Control.Monad.Trans.State.Strict (State, evalState, get, put)
+import Control.Monad.Trans.Reader
 
 type RndState = State StdGen
 
@@ -20,62 +22,70 @@ rndStateToIO rnd = evalState rnd <$> initStdGen
 randomWords :: [[String]]
 randomWords@[ objectPronouns, possessivePronouns, personalPronouns
             , states, nouns, places, when, websites ] =
-  [ ["Her", "Him", "Them"], ["Her", "His", "Their"], ["She", "He", "They"]
-  , [ "California", "Texas", "Florida", "New York", "Pennsylvania"
-    , "Illinois", "Ohio", "Georgia", "North Carolina", "Michigan" ]
-  , [ "Athlete", "Clown", "Shovel", "Paleo Diet", "Doctor", "Parent"
-    , "Cat", "Dog", "Chicken", "Robot", "Video Game", "Avocado"
-    , "Plastic Straw", "Serial Killer", "Telephone Psychic" ]
-  , [ "House", "Attic", "Bank Deposit Box", "School", "Basement"
-    , "Workplace", "Donut Shop", "Apocalypse Bunker" ]
-  , [ "Soon", "This Year", "Later Today", "RIGHT NOW", "Next Week"]
-  , [ "wobsite", "blag", "Facebuuk", "Googles", "Facesbook", "Tweedie"
-    , "Pastagram"] ]
+    [ objectPronouns', possessivePronouns', personalPronouns'
+    , states', nouns', places', when', websites' ]
+  where
+    objectPronouns' = ["Her", "Him", "Them"]
+    possessivePronouns' = ["Her", "His", "Their"]
+    personalPronouns' = ["She", "He", "They"]
+    states' =
+        [ "California", "Texas", "Florida", "New York", "Pennsylvania"
+        , "Illinois", "Ohio", "Georgia", "North Carolina", "Michigan" ]
+    nouns' =
+        [ "Athlete", "Clown", "Shovel", "Paleo Diet", "Doctor", "Parent"
+        , "Cat", "Dog", "Chicken", "Robot", "Video Game", "Avocado"
+        , "Plastic Straw", "Serial Killer", "Telephone Psychic" ]
+    places' =
+        [ "House", "Attic", "Bank Deposit Box", "School", "Basement"
+        , "Workplace", "Donut Shop", "Apocalypse Bunker" ]
+    when' = [ "Soon", "This Year", "Later Today", "RIGHT NOW", "Next Week"]
+    websites' =
+        [ "wobsite", "blag", "Facebuuk", "Googles", "Facesbook", "Tweedie"
+        , "Pastagram"]
 
 selectors :: [RndState String]
 selectors@[ objectPronoun, possessivePronoun, personalPronoun, state
-          , noun, place, aWhen, website ] = select <$> randomWords
-
-intro :: [String]
-intro = [ "Clickbait Headline Generator"
-        , "By Liam Zhu liam.zhu@protonmail.com, adapted from"
-        , "original Python version by Al Sweigart al@inventwithppython.com"
-        , ""
-        , "Our website needs to trick people into looking at ads!" ]
+          , noun, place, aWhen, website ] = select <$> randomWord
 
 headlines :: [RndState String]
 headlines = fmap fold <$>
-    [ (\noun -> ["Are Millenials Killing the ", noun, " Industry?"])
-        <$> noun
-    , (\noun1 noun2 when -> ["Without This ", noun1, ", ", noun2
-      , "s Could Kill You ", when] ) <$> noun <*> noun <*> aWhen
-    , (\objectPronoun state noun1 noun2 -> ["Big Companies Hate "
-      , objectPronoun, "! See How This ", state, " ", noun1
-      , " Invented a Cheaper ", noun2 ] ) <$> objectPronoun <*> state 
-        <*> noun <*> noun
-    , (\state noun possessivePronoun place -> ["You Won't Believe What This "
-      , state, " ", noun, " Found in ", possessivePronoun, " ", place])
-        <$> state <*> noun <*> possessivePronoun <*> place
-    , (\noun1 noun2 -> ["What ", noun1, "s Don't Want You To Know About "
-      , noun2, "s"]) <$> noun <*> noun
-    , (\number noun state -> [show number, " Gift Ideas to Give Your ", noun
-      , " From ", state] ) <$> selectNumber 7 15 <*> noun <*> state
+    [ millennials <$> noun
+    , whatCanKillYou <$> noun <*> noun <*> aWhen
+    , bigCompaniesHate <$> objectPronoun <*> state  <*> noun <*> noun
+    , youWontBelieve <$> state <*> noun <*> possessivePronoun <*> place
+    , dontWantYouToKnow <$> noun <*> noun
+    , giftIdeas <$> selectNumber 7 15 <*> noun <*> state
     , do reasonNum <- selectNumber 3 19
-         (\noun num2 -> [ show reasonNum, " Reasons Why ", noun
-             , " Are More Interesting Than You Think (Number "
-             , show num2, " Will Surprise You!)" ] )
-	     <$> noun <*> selectNumber 1 reasonNum
+         nReasons reasonNum <$> noun <*> selectNumber 1 reasonNum
     , do num <- selectNumber 0 2
-         (\state noun -> ["This ", state, " ", noun
-             , " Didn't Think Robots Would Take "
-             , possessivePronouns !! num, " Job. "
-             , personalPronouns !! num, " "
-             , if num == 2 then "Were" else "Was", " Wrong" ] )
-	     <$> state <*> noun ]
+         didntThinkRobots num <$> state <*> noun
+    ]
+  where
+    millennials noun = ["Are Millennials Killing the ", noun, " Industry?"]
+    whatCanKillYou noun1 noun2 when = [ "Without This ", noun1, ", ", noun2
+        , "s Could Kill You", when ]
+    bigCompaniesHate objectPronoun state noun1 noun2 =
+        ["Big Companies Hate ", objectPronoun, "! See How This ", state, " "
+	, noun1, "Invented a Cheaper ", noun2]
+    youWontBelieve state noun possessivePronoun place =
+        ["You Won't Believe What This ", state, " ", noun, " Found in "
+	, possessivePronoun, " ", place]
+    dontWantYouToKnow noun1 noun2 = ["What ", noun1
+        , "s Don't Want You To Know About ", noun2, "s"]
+    giftIdeas number noun state = [show number, " Gift Ideas to Give Your "
+        , noun, " From ", state]
+    nReasons num1 noun num2 = [ show num1, " Reasons Why ", noun
+        , " Are More Interesting Than You Think (Number ", show num2
+	, " Will Surprise You!)" ]
+    didntThinkRobots num state noun = ["This ", state, " ", noun
+        , "Didn't Think Robots Would Take ", possessivePronouns !! num
+	, " Job. ", personalPronouns !! num, " "
+	, if num == 2 then "Were" else "Was", " Wrong" ]
 
 select :: [a] -> RndState a
-select ls =
-    get >>= fmap (ls !!) . uncurry (<$) . fmap (put $!) . randomR (0, length ls - 1)
+select ls = get
+    >>= fmap (ls !!) . uncurry (<$) 
+      . fmap (put $!) . randomR (0, length ls - 1)
 
 selectNumber :: Int -> Int -> RndState Int
 selectNumber lower upper = get
@@ -83,6 +93,15 @@ selectNumber lower upper = get
 
 main :: IO ()
 main = mapM_ putStrLn intro >> inputLoop
+  where
+    intro =
+        [ "Clickbait Headline Generator"
+        , "By Liam Zhu liam.zhu@protonmail.com, adapted from"
+        , "original Python version by Al Sweigart al@inventwithppython.com"
+        , ""
+        , "Our website needs to trick people into looking at ads!"
+	]
+
 
 inputLoop :: IO ()
 inputLoop = do
@@ -91,6 +110,7 @@ inputLoop = do
     case readMaybe number :: Maybe Int of
         Nothing -> putStrLn "Please enter a number." >> inputLoop
         Just count -> do
+	    putStrLn ""
             replicateM_ count
                 $ putStrLn =<< ( rndStateToIO . join $ select headlines )
             putStrLn ""
